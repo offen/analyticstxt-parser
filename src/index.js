4e4ef2b0c11a2297a0f7be7f6e8f9a4a9f1ff9ea
Frederik Ring <frederik.ring@gmail.com>
2021-04-14 20:22:16 +0200

    improve handling of line numbers on parsing errors

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ const schema = require('./schema')
 
 exports.validate = validate
 function validate (content) {
-  let normalized
-  try {
-    normalized = splitAndNormalize(content)
-  } catch (err) {
-    return err
+  const lines = parse(content.split(/\r?\n/))
+
+  const errors = lines
+    .filter(l => l.error)
+    .map(l => l.line)
+  if (errors.length) {
+    return new Error(
+      `Could not parse following line(s): ${errors.join(', ')}`
+    )
   }
+
+  const normalized = lines
+    .filter(l => !l.isCommentOrEmpty)
+    .reduce((acc, line) => {
+      acc[line.field] = line.values
+      return acc
+    }, {})
+
   const ajv = new Ajv()
   const validate = ajv.compile(schema)
+
   const valid = validate(normalized)
   if (!valid) {
     return validate.errors
@@ -19,17 +32,45 @@ function validate (content) {
   return null
 }
 
-function splitAndNormalize (input) {
+function parse (input) {
   return input
-    .split(/\r?\n/)
-    .map(function (line) { return line.trim() })
-    .filter(function (line) { return line.indexOf('#') !== 0 && line !== '' })
-    .map(function (line) { return line.split(':') })
-    .reduce(function (acc, line) {
-      if (line.length !== 2) {
-        throw new Error(`Cannot parse line "${line.join(':')}".`)
+    .map(line => line.trim())
+    .map((line, index) => {
+      const result = {
+        line: index + 1,
+        error: null,
+        field: null,
+        values: null,
+        isCommentOrEmpty: false
       }
-      acc[line[0].toLowerCase()] = line[1].split(',').map(s => s.trim().toLowerCase())
-      return acc
-    }, {})
+
+      if (line.indexOf('#') === 0 || line === '') {
+        return {
+          ...result,
+          isCommentOrEmpty: true
+        }
+      }
+
+      const [field, value] = line.split(':')
+      if (!field || !value) {
+        const error = new Error(
+          `Cannot parse line ${index + 1}, no field name or value found.`
+        )
+        return {
+          ...result,
+          error
+        }
+      }
+
+      const values = value
+        .split(',')
+        .filter(Boolean)
+        .map(s => s.trim().toLowerCase())
+
+      return {
+        ...result,
+        field: field.toLowerCase(),
+        values
+      }
+    })
 }
