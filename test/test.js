2c0a241f63706585ecbe4da9b23e9587457293c1
Frederik Ring <frederik.ring@gmail.com>
2021-04-15 11:56:49 +0200

    set up continuous integration

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const tape = require('tape')
 
 const { validate } = require('./../src/index')
 
-tape.test('valid', t => {
+tape.test('valid: full definition', t => {
   const fixture = `
 # analytics.txt file for www.analyticstxt.org
 Author: Frederik Ring <hioffen@posteo.de>
@@ -29,12 +29,42 @@ Deploys: offen
   t.end()
 })
 
-tape.test('valid empty', t => {
+tape.test('valid: empty', t => {
   const fixture = `
 Author: Frederik Ring <hioffen@posteo.de>
 Collects: none
 `
   const result = validate(fixture)
   t.equal(result, null)
+  t.end()
+})
+
+tape.test('invalid: parse error', t => {
+  const fixture = `
+{"json": "is cool"}
+`
+  const result = validate(fixture)
+  t.notEqual(result, null)
+  t.end()
+})
+
+tape.test('invalid: definition error', t => {
+  const fixture = `
+Author: Frederik Ring <hioffen@posteo.de>
+Collects: url, referrer, device-type
+Stores: first-party-cookies, local-storage
+Uses: javascript
+# none is not allowed as a value of many
+Allows: opt-in, opt-out, none
+Retains: P6M
+Honors: none
+Tracks: sessions, users
+Varies: none
+Shares: per-user
+Implements: gdpr
+Deploys: offen
+`
+  const result = validate(fixture)
+  t.notEqual(result, null)
   t.end()
 })
