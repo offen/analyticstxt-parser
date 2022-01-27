1b78b3dbbba11d344526bed654928fa8e81b47a6
Frederik Ring <frederik.ring@gmail.com>
2022-01-27 14:23:45 +0100

    Adjust to draft-03

--- schema/index.js
+++ schema/index.js
@@ -1,4 +1,5 @@
 exports['draft-ring-analyticstxt-00'] = require('./draft-ring-analyticstxt-00')
 exports['draft-ring-analyticstxt-01'] = require('./draft-ring-analyticstxt-01')
 exports['draft-ring-analyticstxt-02'] = require('./draft-ring-analyticstxt-02')
-exports.defaultVersion = 'draft-ring-analyticstxt-02'
+exports['draft-ring-analyticstxt-03'] = require('./draft-ring-analyticstxt-03')
+exports.defaultVersion = 'draft-ring-analyticstxt-03'
