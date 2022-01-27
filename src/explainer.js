e4f6a7eed49bb3bfea202fb131119efe45cd0650
Frederik Ring <frederik.ring@gmail.com>
2022-01-27 12:04:57 +0100

    Tweak explainer content according to feedback

--- src/explainer.js
+++ src/explainer.js
@@ -21,12 +21,14 @@ module.exports = function (data) {
 function renderIntro (Author) {
   const result = new Block()
   result.paragraph(`
-The following is a short summary about an analytics.txt file authored
-by ${Author.values[0]}, focusing on the user-facing consequences of the
-collection of usage data. Website operators collect usage data to measure
+Website operators collect usage data to measure
 user engagement with their sites and services, identify possible issues and
 improve user experience. The degree to which this affects users' privacy can
 vary drastically.
+
+The following is a short summary about an analytics.txt file authored
+by ${Author.values[0]}, focusing on the user-facing consequences of the
+collection of usage data.
 `)
   return result.toString()
 }
@@ -42,15 +44,15 @@ function renderCollects (Collects) {
   result.push('On a visit, the site collects the following data about your visit:')
   const items = {
     url: 'The URL of the page you are visiting. In almost all cases, this will also include a timestamp.',
-    'ip-address': 'The IP address you are currently using. This might be shared with other users that are on the same network as you.',
+    'ip-address': 'The IP address you are currently using. This address might be shared with other users that are on the same network as you. It can be used for reidentication without storing any data on your system.',
     'geo-location': 'Your geographic location. Depending on the technology used for collecting this information, the resolution might be as low as on country level or very detailed.',
     'user-agent': 'The User Agent string which discloses information about the browser and the operating system you are using.',
     fingerprint: 'A fingerprint is a combination of multiple charactersistics of the device you are using. This can be used to uniquely identify the device without having to use client-side storage like cookies.',
-    'device-type': 'The type of your device, i.e. whether it is a mobile device, a tablet or a desktop device. The used buckets will be specific to the tool used.',
+    'device-type': 'The type of your device, i.e. whether it is a mobile device, a tablet or a desktop device. The exact classification will be specific to the tool used.',
     referrer: 'In case you are visiting the site by referral from another site, the source of the referral is being collected.',
     'visit-duration': 'The time spent on the website.',
-    'custom-events': 'Custom events are recorded on certain actions, e.g. when you subscribe to a newsletter or similar.',
-    'session-recording': 'Your session on this site is recorded in its entirety. This includes scrolling, mouse movement and any other interaction with the site.'
+    'custom-events': 'Custom events are generated on certain user actions, e.g. when you subscribe to a newsletter or similar.',
+    'session-recording': 'Your session on the site is recorded in its entirety. This includes scrolling, mouse movement and any other interaction with the site.'
   }
   for (const value of values) {
     result.push(`- ${items[value]}`)
@@ -86,7 +88,7 @@ function renderUses (Uses) {
   }
   result.push('The following technologies are used to collect data about your visit:')
   const items = {
-    javascript: 'The site is using a client side script running on your device to collect data. This allows for collecting a broad range of information about your device and usage patterns.',
+    javascript: 'The site is using a client side script running on your device to collect data. This theoretically allows for collecting a broad range of information about your device and usage patterns. The Collects section contains further information about what exactly is being collected.',
     pixel: 'The site is using a so-called "tracking pixel" to collect data. This delivers a limited set of data but is very robust and difficult to block.',
     'server-side': 'The site is using server-side technology (e.g. server logs) to collect usage data. The set of data is limited, however this technology cannot be blocked by clients in any way.',
     other: 'The site is using other means of collecting usage data. You can check the comments of the analytics.txt file for further information.'
@@ -113,7 +115,7 @@ function renderAllows (Allows) {
 
   result.push('The site allows for the following actions regarding your consent decision:')
   const items = {
-    'opt-in': 'The site does not collect usage data before you actively give consent.',
+    'opt-in': 'The site allows you to opt in to the collection of usage data. No usage data is collected before you have actively given consent.',
     'opt-out': 'The site allows you to opt out of data collection at any time.'
   }
   for (const value of values) {
